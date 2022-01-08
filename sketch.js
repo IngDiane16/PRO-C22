f71a84f9ce1a1a0de77e48950fec671e7e23ec1e
Diana <ing.diane.bb@gmail.com>
2022-01-08 17:55:14 -0600

    added a tower

--- sketch.js
+++ sketch.js
@@ -2,32 +2,37 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
-var engine, world,ground;
-var backgroundImg;
-
+var engine, world, ground;
+var backgroundImg, tower, towerImage;
 
 function preload() {
-  backgroundImg=loadImage("./assets/background.gif");
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
 }
 function setup() {
-  
   canvas = createCanvas(1200, 600);
   engine = Engine.create();
   world = engine.world;
-  var options={
-    isStatic:true,
-  }
-  ground= Bodies.rectangle(0,height-1,width*2,1,options);
-  World.add(world,ground);
-  
- 
+  var options = {
+    isStatic: true,
+  };
+  ground = Bodies.rectangle(0, height - 1, width * 2, 1, options);
+  World.add(world, ground);
+
+  tower = Bodies.rectangle(160, 350, 160, 310, options);
+  World.add(world, tower);
 }
 
 function draw() {
   background(189);
- 
+
   Engine.update(engine);
-  rect(ground.position.x,ground.position.y,width*2,1);
-  image(backgroundImg,0,0,1200,600);
-   
+  rect(ground.position.x, ground.position.y, width * 2, 1);
+  image(backgroundImg, 0, 0, 1200, 600);
+  push();
+  rectMode(CENTER);
+  rect(towerImage, tower.position.x, tower.position.y, 160, 310);
+  pop();
+
+  
 }
