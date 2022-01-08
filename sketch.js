d2bb401c35acf701ad48a83e6bad01ef16be4aa8
Diana <ing.diane.bb@gmail.com>
2022-01-08 17:36:33 -0600

    added a new background

--- sketch.js
+++ sketch.js
@@ -3,17 +3,22 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 var engine, world,ground;
-
+var backgroundImg;
 
 
 function preload() {
- 
+  backgroundImg=loadImage("./assets/background.gif");
 }
 function setup() {
-
+  
   canvas = createCanvas(1200, 600);
   engine = Engine.create();
   world = engine.world;
+  var options={
+    isStatic:true,
+  }
+  ground= Bodies.rectangle(0,height-1,width*2,1,options);
+  World.add(world,ground);
   
  
 }
@@ -22,6 +27,7 @@ function draw() {
   background(189);
  
   Engine.update(engine);
-  
+  rect(ground.position.x,ground.position.y,width*2,1);
+  image(backgroundImg,0,0,1200,600);
    
 }
